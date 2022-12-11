c63fc2216bed10cbf6f9ff5527f8f13b63c36ddd
smthzch <ezke.smith@gmail.com>
2022-12-11 16:27:11 -0500

    day 11...

--- advent.py
+++ advent.py
@@ -10,54 +10,61 @@ welcome = """######################################
 """
 print(welcome)
 
-print("---Day 1---")
+printd = lambda x: print(f"---Day {x}---")
 
+printd(1)
 pth = "data/day{}.txt"
 elf_cals = day1.get_elf_cals(pth.format(1))
 elf_cals.sort()
 print(f"Max cals :{ elf_cals[-1] }")
 print(f"Top 3 cals :{ sum(elf_cals[-3:]) }")
 
-print("---Day 2---")
+printd(2)
 rps_score = day2.get_score1(pth.format(2))
 print(f"Total score :{ rps_score }")
 rps_score = day2.get_score2(pth.format(2))
 print(f"Total score :{ rps_score }")
 
-print("---Day 3---")
+printd(3)
 sack = day3.ElfSack(pth.format(3))
 sack.get_duplicate_priorities()
 sack.get_badge_priorities()
 
-print("---Day 4---")
+printd(4)
 groups = day4.ElfGroup(pth.format(4))
 groups.count_contained()
 groups.count_overlaps()
 
-print("---Day 5---")
+printd(5)
 crates = day5.Crates(9, "data/day5_stack.txt")
 crates.move_one("data/day5_moves.txt")
 crates.move_all("data/day5_moves.txt")
 
-print("---Day 6---")
+printd(6)
 signal = day6.ElfSignal(pth.format(6))
 signal.find_start()
 
-print("---Day 7---")
+printd(7)
 fs = day7.FileSystem(pth.format(7))
 fs.sum_dirs()
 fs.to_delete()
 
-print("---Day 8---")
+printd(8)
 treehouse = day8.TreeHouse(pth.format(8))
 treehouse.count_visible()
 treehouse.max_scenic_score()
 
-print("--- Day 9---")
+printd(9)
 rope = day9.Rope(pth.format(9))
 #rope.find_visited(n_knots=2)
 #rope.find_visited(n_knots=10)
 
-print("---Day 10---")
+printd(10)
 crt = day10.CRT(pth.format(10))
 crt.run_program()
+
+printd(11)
+monkeys = day11.Monkeys(pth.format(11))
+monkeys.monkey_business(rounds=20, relief=True)
+monkeys = day11.Monkeys(pth.format(11))
+monkeys.monkey_business(rounds=10_000, relief=False)
---
+++ treeofknowledge/day11.py
@@ -0,0 +1,69 @@
+from math import floor
+
+class Monkey:
+    def __init__(self, id, item_line, operator_line, test_lines):
+        self.id = id
+        self.items = [ int(x) for x in item_line.split(":")[1].split(",") ]
+        op = "lambda old:" + operator_line.split(":")[1].split("=")[1]
+        self.operation = eval(op)
+        
+        self.modulo = int(test_lines[0].split(" ")[-1])
+        true_monkey = int(test_lines[1].split(" ")[-1])
+        false_monkey = int(test_lines[2].split(" ")[-1])
+        self.test = lambda x: (true_monkey, True) if x % self.modulo == 0 else (false_monkey, False)
+
+        self.items_inspected = 0
+
+    def inspect_items(self, relief, hcf):
+        items = []
+        while len(self.items) > 0:
+            item = self.items.pop(0)
+            new_item = self.operation(item)
+            if relief:
+                new_item = floor(new_item / 3)
+            else:
+                new_item = new_item % hcf
+
+            to_monkey, test_result = self.test(new_item)
+            items += [(new_item, to_monkey)]
+            self.items_inspected += 1
+
+        return items
+
+    def take(self, item):
+        self.items += [item]
+
+class Monkeys:
+    def __init__(self, pth):
+        self.monkeys = []
+        self.hcf = 1
+
+        with open(pth) as rdr:
+            line = rdr.readline()
+            while line:
+                self.parse_monkey(rdr)
+                line = rdr.readline()
+
+    def monkey_business(self, rounds, relief):
+        for r in range(rounds):
+            for monkey in self.monkeys:
+                for item, to_monkey in monkey.inspect_items(relief, self.hcf):
+                    self.monkeys[to_monkey].take(item)
+
+        for m in self.monkeys:
+            print(m.items_inspected)
+        
+        items_inspected = [m.items_inspected for m in self.monkeys]
+        items_inspected.sort()
+        monkey_business = items_inspected[-1] * items_inspected[-2]
+        print(f"Total monkey business: {monkey_business}")
+
+    def parse_monkey(self, rdr):
+        item_line = rdr.readline().strip()
+        operation_line = rdr.readline().strip()
+        test_lines = [ rdr.readline().strip(), rdr.readline().strip(), rdr.readline().strip() ]
+        rdr.readline() # dump empty line
+
+        new_monkey = Monkey(len(self.monkeys), item_line, operation_line, test_lines)
+        self.monkeys += [ new_monkey ]
+        self.hcf *= new_monkey.modulo
